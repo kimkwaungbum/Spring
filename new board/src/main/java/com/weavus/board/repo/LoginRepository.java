3226bc1a50aa267f54579f45921a29298d05fd6c
kimkwaungbum <148036354+kimkwaungbum@users.noreply.github.com>
2023-12-01 14:47:20 +0900

    Add files via upload

---
+++ new board/src/main/java/com/weavus/board/repo/LoginRepository.java
@@ -0,0 +1,13 @@
+package com.weavus.board.repo;
+
+import com.weavus.board.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface LoginRepository extends JpaRepository<Member,String> {
+
+    @Query(value ="select * from member where id = ? and password = ?",nativeQuery = true)
+    public Member findByIdAndPw(String id, String password);
+
+
+}
